ad2b5051bc4df28325687ce2e35ac53a9a8dff00
Matteo Gentili <103628530+MatteoGentili@users.noreply.github.com>
2023-10-21 10:48:45 +0200

    update + début ajout homme machine

--- Python_files/main.py
+++ Python_files/main.py
@@ -4,7 +4,7 @@
 ######################################
 ######################################
 import pandas as pd
-
+from datetime import datetime
 ######################################
 ######################################
 ############ Variables ###############
@@ -42,7 +42,8 @@ while True:
     matches_depart = [nom_gare for nom_gare in noms_gares if gare_depart_user in nom_gare.lower()]
 
     if len(matches_depart) == 1:
-        break  # Sortez de la boucle, car l'entrée est valide
+        gare_depart_correspondante = matches_depart[0]
+        break
     elif len(matches_depart) > 1:
         print("Gares de départ valides :")
         for match in matches_depart:
@@ -50,20 +51,58 @@ while True:
     else:
         print("Aucune correspondance trouvée pour la gare de départ. Essayez à nouveau.")
 
-# Trouver toutes les gares d'arrivée possibles depuis la gare de départ choisie
-gares_arrivee_possibles = set()
-for index, row in df_retard_gares.iterrows():
-    gare_depart = row['gare_depart'].lower()
-    gare_arrivee = row['gare_arrivee'].lower()
+# Filtrer le DataFrame en fonction de la gare de départ
+filtre = df_retard_gares['gare_depart'].str.lower() == gare_depart_correspondante.lower()
+gares_arrivee_uniques = df_retard_gares.loc[filtre, 'gare_arrivee'].unique()
 
-    if gare_depart == matches_depart[0].lower:
-        gares_arrivee_possibles.add(gare_arrivee)
-        print(gares_arrivee_possibles)
+# Afficher la liste des gares d'arrivée uniques
+if gares_arrivee_uniques.any():
+    print("\nGares d'arrivée uniques depuis", gare_depart_correspondante, ":")
+    for i, gare in enumerate(gares_arrivee_uniques, start=1):
+        print(f"{i}. {gare}")
 
-# Afficher la liste des gares d'arrivée possibles
-if gares_arrivee_possibles:
-    print("Gares d'arrivée possibles depuis", matches_depart[0], ":")
-    for gare in gares_arrivee_possibles:
-        print("- ", gare)
+    # Demander à l'utilisateur de choisir une gare d'arrivée
+    while True:
+        choix = input("\nChoisissez une gare d'arrivée en entrant son numéro (ou 'q' pour quitter) : ")
+        if choix.lower() == 'q':
+            break  # Quitter le programme
+        try:
+            choix = int(choix)
+            if 1 <= choix <= len(gares_arrivee_uniques):
+                gare_arrivee_choisie = gares_arrivee_uniques[choix - 1]
+                # Demander à l'utilisateur de choisir une date au format "année-mois" (par exemple, "2018-01")
+                while True:
+                    date_user = input("Entrez la date au format 'année-mois' (par exemple, '2018-01') : ").strip()
+
+                    # Vérifier que la date est au bon format (année-mois)
+                    try:
+                        year, month = map(int, date_user.split('-'))
+                        if (1 <= month <= 12 and year > datetime.now().year) or (year == datetime.now().year and month > datetime.now().month):
+                            break  # Date valide
+                        else:
+                            print("Date invalide ou antérieure à la date actuelle. Essayez à nouveau.")
+                    except ValueError:
+                        print("Format de date incorrect. Utilisez le format 'année-mois' (par exemple, '2018-01').")
+                filtre_trajet = (df_retard_gares['gare_depart'].str.lower() == gare_depart_correspondante.lower()) & \
+                                (df_retard_gares['gare_arrivee'].str.lower() == gare_arrivee_choisie.lower())
+                durees_moyennes = df_retard_gares.loc[filtre_trajet, 'duree_moyenne']
+                if not durees_moyennes.empty:
+                    somme_durees = durees_moyennes.sum()
+                    moyenne_duree = somme_durees / len(durees_moyennes)
+                    heure=int(moyenne_duree//60)
+                    minute=int(moyenne_duree%60)
+                
+                # Maintenant, vous pouvez utiliser la date choisie pour effectuer des opérations supplémentaires
+                print(f"\nVous avez choisi comme trajet : {gare_depart_correspondante} - {gare_arrivee_choisie}")
+                print(f"Pour la date : {date_user}")
+                print(f"Le trajet durera en moyenne : {heure} heures et {minute} minutes\n")
+
+                break  # Sortir de la boucle
+            else:
+                print("Numéro invalide. Veuillez réessayer.")
+        except ValueError:
+            print("Saisie invalide. Veuillez entrer un numéro valide.")
 else:
-    print("Aucune gare d'arrivée trouvée depuis", matches_depart[0])
+    print("Aucune gare d'arrivée trouvée depuis", gare_depart_correspondante)
+
+print('Prédisons le retard pour votre trajet ....\n')
